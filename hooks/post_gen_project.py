32b7d1a53724dcd423b6237d8914aac1e23ac52f
jcook3701 <jcook3701+github@gmail.com>
2025-12-30 03:28:09 -0800

    Update template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -21,8 +21,7 @@ import json
 import os
 
 from nutrimatic.core import make
-from nutrimatic.hooks.post_gen_logic import (    generate_ansible_dirs,
-    generate_docs_templates,
+from nutrimatic.hooks.post_gen_logic import (    generate_docs_templates,
     get_make_cmds,
 )
 
@@ -37,8 +36,7 @@ def main() -> None:
 
     # Access cookiecutter context safely
     context = json.loads("""{{ cookiecutter | jsonify }}""")
-    generate_docs_templates(context)    generate_ansible_dirs()
-
+    generate_docs_templates(context)
     # Run make commands to get project seeded
     make_cmds: list[str] = get_make_cmds(context)
 
